5f5a0dbdf6e643add1c0042ab56dc4e3a90696a5
Mohd Dawood <serious17org@gmail.com>
2024-10-26 20:58:19 +0530

    Improved Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,7 @@ import style from "../styles/Nav.module.scss";
 import { useAtom } from "jotai";
 import { weatherData } from "./states";
 import fetchData from "./fetchData";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 const Nav = () => {
   const [weather, setWeather] = useAtom(weatherData);
@@ -18,14 +19,19 @@ const Nav = () => {
   return (
     <nav>
       <h1>Breeze Aura</h1>
+
       <form onSubmit={searchLocation}>
-        <input
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          type="text"
-          required
-        />
-        <button type="submit">Search</button>
+        <div className={style.searchBar}>
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            required
+          />
+          <button type="submit">
+            <Icon icon="basil:search-outline" width="1.7rem" height="1.7rem" />
+          </button>
+        </div>
       </form>
     </nav>
   );
