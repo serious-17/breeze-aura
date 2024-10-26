edc7fd34d0bde198e1d3fed274e6840d8a3bc2db
Mohd Dawood <serious17org@gmail.com>
2024-10-26 19:23:23 +0530

    fade Animations

--- src/page/Home.js
+++ src/page/Home.js
@@ -3,7 +3,7 @@ import fetchData from "../components/fetchData";
 import { useAtom } from "jotai";
 import { weatherData } from "../components/states";
 import style from "../styles/Home.module.scss";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { Icon } from "@iconify/react/dist/iconify.js";
 
 const Home = () => {
@@ -24,99 +24,132 @@ const Home = () => {
     return time;
   };
 
+  const fade = {
+    hidden: {
+      opacity: 0,
+    },
+    show: {
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    exit: {
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
-    <motion.div className={style.background}>
+    <motion.div
+      initial="hidden"
+      animate="show"
+      exit="exit"
+      className={style.background}
+    >
       {!weather.results && !weather.isLoading && (
-        <h1 className={style.loading}>no results were found</h1>
+        <AnimatePresence mode="wait">
+          <motion.h1 variants={fade} className={style.loading}>
+            no results were found
+          </motion.h1>
+        </AnimatePresence>
       )}
       {weather.isLoading && (
-        <Icon
-          className={style.loading}
-          icon="svg-spinners:pulse-multiple"
-          width="128px"
-          height="128px"
-        />
+        <AnimatePresence mode="wait">
+          <motion.div variants={fade}>
+            <Icon
+              className={style.loading}
+              icon="svg-spinners:pulse-multiple"
+              width="128px"
+              height="128px"
+            ></Icon>
+          </motion.div>
+        </AnimatePresence>
       )}
       {!weather.isLoading && weather.results && (
-        <div className="weather">
-          <div className={style.mainInfo}>
-            <div className={style.weather}>
-              <motion.h2
-                drag="x"
-                dragConstraints={{
-                  left: 0,
-                  right: 0,
-                }}
-              >
-                {weather.current.temp_c}º C
-              </motion.h2>
-              <div className={style.condition}>
-                <p>{weather.current.condition.text}</p>
-                <img src={weather.current.condition.icon} alt="" />
-              </div>
-              <div className={style.title}>
-                <h3>
-                  {weather.location.name}, {weather.location.region}
-                </h3>
-                <p>{weather.location.country}</p>
-              </div>
-            </div>
-          </div>
-          <div className={style.extraInfo}>
-            <div className={style.cards}>
-              <div className={style.card}>
-                <h4>Humidity</h4>
-                <p>{weather.current.humidity}%</p>
-              </div>
-              <div className={style.card}>
-                <h4>UV</h4>
-                <p>{weather.current.uv}</p>
-              </div>
-              <div className={style.card}>
-                <h4>Wind</h4>
-                <p>
-                  {weather.current.wind_kph} km/h {weather.current.wind_dir}
-                </p>
-              </div>
-              <div className={style.card}>
-                <h4>Pressure</h4>
-                <p>{weather.current.pressure_mb}mbar</p>
-              </div>
-              <div className={style.card}>
-                <h4>Real Feel</h4>
-                <p>{weather.current.feelslike_c}º C</p>
-              </div>
-              <div className={style.card}>
-                <h4>Heat Index</h4>
-                <p>{weather.current.heatindex_c}º C</p>
-              </div>
-              <div className={style.card}>
-                <h4>Sunrise</h4>
-                <p>{weather.astro.sunrise}</p>
-              </div>
-              <div className={style.card}>
-                <h4>Sunset</h4>
-                <p>{weather.astro.sunset}</p>
-              </div>
-              <div className={style.card}>
-                <h4>Current Time</h4>
-                <p>{getTime()}</p>
-              </div>
-              <div className={style.card}>
-                <h4>Moonrise</h4>
-                <p>{weather.astro.moonrise}</p>
-              </div>
-              <div className={style.card}>
-                <h4>Moonset</h4>
-                <p>{weather.astro.moonset}</p>
-              </div>
-              <div className={style.card}>
-                <h4>Moon Phase</h4>
-                <p>{weather.astro.moon_phase}</p>
+        <AnimatePresence mode="wait">
+          <motion.div variants={fade} className="weather-container">
+            <div className={style.mainInfo}>
+              <div className={style.weather}>
+                <motion.h2
+                  drag="x"
+                  dragConstraints={{
+                    left: 0,
+                    right: 0,
+                  }}
+                >
+                  {weather.current.temp_c}º C
+                </motion.h2>
+                <div className={style.condition}>
+                  <p>{weather.current.condition.text}</p>
+                  <img src={weather.current.condition.icon} alt="" />
+                </div>
+                <div className={style.title}>
+                  <h3>
+                    {weather.location.name}, {weather.location.region}
+                  </h3>
+                  <p>{weather.location.country}</p>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+            <div className={style.extraInfo}>
+              <div className={style.cards}>
+                <div className={style.card}>
+                  <h4>Humidity</h4>
+                  <p>{weather.current.humidity}%</p>
+                </div>
+                <div className={style.card}>
+                  <h4>UV</h4>
+                  <p>{weather.current.uv}</p>
+                </div>
+                <div className={style.card}>
+                  <h4>Wind</h4>
+                  <p>
+                    {weather.current.wind_kph} km/h {weather.current.wind_dir}
+                  </p>
+                </div>
+                <div className={style.card}>
+                  <h4>Pressure</h4>
+                  <p>{weather.current.pressure_mb}mbar</p>
+                </div>
+                <div className={style.card}>
+                  <h4>Real Feel</h4>
+                  <p>{weather.current.feelslike_c}º C</p>
+                </div>
+                <div className={style.card}>
+                  <h4>Heat Index</h4>
+                  <p>{weather.current.heatindex_c}º C</p>
+                </div>
+                <div className={style.card}>
+                  <h4>Sunrise</h4>
+                  <p>{weather.astro.sunrise}</p>
+                </div>
+                <div className={style.card}>
+                  <h4>Sunset</h4>
+                  <p>{weather.astro.sunset}</p>
+                </div>
+                <div className={style.card}>
+                  <h4>Current Time</h4>
+                  <p>{getTime()}</p>
+                </div>
+                <div className={style.card}>
+                  <h4>Moonrise</h4>
+                  <p>{weather.astro.moonrise}</p>
+                </div>
+                <div className={style.card}>
+                  <h4>Moonset</h4>
+                  <p>{weather.astro.moonset}</p>
+                </div>
+                <div className={style.card}>
+                  <h4>Moon Phase</h4>
+                  <p>{weather.astro.moon_phase}</p>
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        </AnimatePresence>
       )}
     </motion.div>
   );
